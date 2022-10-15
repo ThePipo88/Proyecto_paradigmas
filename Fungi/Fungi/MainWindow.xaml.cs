f654df3a1be973b1d10bf12befded78f2dd0b38a
ReynerBadillaArias <reyner.badilla.arias@est.una.ac.cr>
2022-10-14 21:37:01 -0600

    Interfaz grafica

--- Fungi/Fungi/MainWindow.xaml.cs
+++ Fungi/Fungi/MainWindow.xaml.cs
@@ -20,17 +20,60 @@ namespace Fungi
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Title = "Fungi";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            lbNombre.Content = "hi";
+            
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fileSpaceBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var caretIndex = fileSpaceBox.CaretIndex;
+                fileSpaceBox.Text = fileSpaceBox.Text.Insert(caretIndex, "\n");
+                fileSpaceBox.CaretIndex = caretIndex + 1;
+            }
+        }
+
+        private void fileSpaceBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void fileSpaceBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void fileSpaceNumber_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtOutput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void fileSpaceBox_1(object sender, TextChangedEventArgs e)
         {
 
         }
